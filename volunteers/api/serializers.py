94037f8fe5fcfc85784e9c483c200ff718eed142
Varvara Korableva <v.korableva1990@yandex.ru>
2023-12-15 22:54:11 +0200

    views, serializers,urls added

---
+++ volunteers/api/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from assist.models import Action, Volunteer, ActionVolunteer, ActionCompany
+
+
+class ActionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Action
+        fields = ('id', 'name', 'description', 'valid_from', 'valid_till')
+        read_only_fields = ('id',)
+
+
+class VolunteerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Volunteer
+        fields = ('id', 'first_name', 'last_name', 'email', 'telegram', 'sex')
+        read_only_fields = ('id',)    
+
+
+class ActionVolunteerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ActionVolunteer
+        fields = ('id',)
+        read_only_fields = ('id',)   
+
+
+'''
+class ActionCompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ActionCompany
+        fields = ('id')
+        read_only_fields = ('id',)                   
+'''
